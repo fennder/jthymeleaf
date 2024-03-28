1f19a183699937b2cbd567f61f862db5387ab715
Edir P Santos <fennder@gmail.com>
2024-03-28 19:05:30 -0300

    First Commit for project thymeleaf

---
+++ src/main/java/br/dev/abrindoportas/jthymeleaf/JthymeleafApplication.java
@@ -0,0 +1,13 @@
+package br.dev.abrindoportas.jthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JthymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JthymeleafApplication.class, args);
+	}
+
+}
