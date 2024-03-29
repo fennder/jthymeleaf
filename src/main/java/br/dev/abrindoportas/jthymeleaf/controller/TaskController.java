aeff8201a15783b8d962617f425f75f75ee3e9b9
Edir P Santos <fennder@gmail.com>
2024-03-28 21:32:19 -0300

    Create method PostCreateString, Update GetHomeString and Array List for create objects

--- src/main/java/br/dev/abrindoportas/jthymeleaf/controller/TaskController.java
+++ src/main/java/br/dev/abrindoportas/jthymeleaf/controller/TaskController.java
@@ -1,15 +1,30 @@
 package br.dev.abrindoportas.jthymeleaf.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import br.dev.abrindoportas.jthymeleaf.models.TaskModel;
+
 
 
 @Controller
 public class TaskController {
   
+  List<TaskModel> taskModels = new ArrayList<>();
+
   @GetMapping("/create")
-  public String getCreateString() {
+  public String getHomeString() {
       return "create";
   }
   
+  @PostMapping("/create")
+  public void postCreateString(TaskModel taskModel) {
+    // System.out.println("O nome da tarefa é " + taskModel.getName());
+    Long id = taskModels.size() + 1L;
+    taskModels.add(new TaskModel(id, taskModel.getName(), taskModel.getDate()))
+  }
 }
